e408fddcda6cc4492cb00a964a1cdf100087b775
Derek Zhu <1and1get2@gmail.com>
2019-07-03 15:43:08 +1200

    updated flag

--- androidutil/src/main/java/com/github/hereisderek/androidutil/util/flag/Flag.kt
+++ androidutil/src/main/java/com/github/hereisderek/androidutil/util/flag/Flag.kt
@@ -2,25 +2,58 @@ package com.github.hereisderek.androidutil.util.flag
 
 import java.util.concurrent.atomic.AtomicInteger
 
-inline class Flag(val value: Int) {
+class Flag(var value: Int = 0) {
     constructor(vararg flags: Int) : this(sum(*flags))
     constructor(flags: List<Int>) : this(sum(flags))
 
 
-    fun hasFlag(flag: Flag) = flag.value and this.value == flag.value
-    fun withFlag(flag: Flag) = this.value or flag.value
-    fun minusFlag(flag: Flag) = this.value and flag.value.inv()
+    fun hasFlag(flag: Flag) : Boolean = has(this.value, flag.value)
+    fun withFlag(flag: Flag) : Flag = Flag(with(value, flag.value))
+    fun minusFlag(flag: Flag) : Flag = Flag(value, flag.value)
 
-    fun hasFlag(flag: Int) = flag and this.value == flag
-    fun withFlag(flag: Int) = this.value or flag
-    fun minusFlag(flag: Int) = this.value and flag.inv()
+    fun hasFlag(flag: Int) : Boolean = has(this.value, flag)
+    fun withFlag(flag: Int) : Flag = Flag(with(value, flag))
+    fun minusFlag(flag: Int) : Flag = Flag(value, flag)
+
+
 
     operator fun minus(flag: Int) = minusFlag(flag)
 
     operator fun plus(flag: Int) = withFlag(flag)
 
+    operator fun plusAssign(flag: Int) {
+        value = with(value, flag)
+    }
+
+    operator fun minusAssign(flag: Int) {
+        value = minus(value, flag)
+    }
+
+
+
+    operator fun minus(flag: Flag) = minus(flag.value)
+
+    operator fun plus(flag: Flag): Flag = withFlag(flag.value)
+
+    operator fun plusAssign(flag: Flag) {
+        plusAssign(flag.value)
+    }
+
+    operator fun minusAssign(flag: Flag) {
+        minusAssign(flag.value)
+    }
+
+
+
     companion object {
         val MAX_FLAG = Flag(Int.MAX_VALUE)
+
+
+        private fun minus(from: Int, subtract: Int) = from and subtract.inv()
+
+        private fun with(from: Int, addition: Int) = from or addition
+
+        private fun has(from: Int, contains: Int) = from and contains == contains
 
         private fun sum(vararg flags: Int) : Int {
             var sum = 0
